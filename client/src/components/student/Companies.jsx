2a0972c18f7cbfbf9561d958e1313b730069895a
monishreeramesh <monishreemoni2000@gmail.com>
2026-01-20 12:13:20 +0530

    changes_added

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -5,6 +5,7 @@ const Companies = () => {
   return (
     <div className="pt-16">
       <p className="text-base text-gray-500">Trusted by learners from</p>
+      <marquee behavior="scroll" direction="left">
       <div className="flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5">
         <img className='md:w-28 w-20' src={assets.microsoft_logo} alt="Microsoft" />
         <img className='md:w-28 w-20' src={assets.walmart_logo} alt="Walmart" />
@@ -12,6 +13,7 @@ const Companies = () => {
         <img className='md:w-24 w-20' src={assets.adobe_logo} alt="Adobe" />
         <img className='md:w-24 w-20' src={assets.paypal_logo} alt="Paypal" />
       </div>
+      </marquee>
     </div>
   );
 };
